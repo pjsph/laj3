80b37cc006d72b434b30b6ba92f05117e10cf0b6
pjsph <42893181+pjsph@users.noreply.github.com>
2024-05-28 20:10:44 +0200

    Add stdout printing and recursive behaviour

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::{Parser, Subcommand};
-use std::{collections::HashMap, fs::{self, read_dir}, path::Path};
+use std::{collections::HashMap, fmt::Display, fs::{self, read_dir}, hash::Hash, io::{self, Write}, path::Path};
 
 #[derive(Parser)]
 #[command(name = "laj3", version, about)]
@@ -11,14 +11,63 @@ struct Cli {
 #[derive(Subcommand)]
 enum Commands {
     Dict { 
-        #[arg(short, long, default_value_t = String::from("out.map"))]
+        #[arg(short, long)]
         #[arg(help = "Output file to store the dictionary")]
-        output: String,
+        output: Option<String>,
 
         #[arg(short, long, default_value_t = false)]
         #[arg(help = "Compute dictionary for subdirectories")]
         recursive: bool
     },
+}
+
+#[derive(Debug, Clone)]
+struct HashError(String);
+
+impl Display for HashError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+fn hash_file(path: &Path) -> Result<String, HashError> {
+    if path.is_file() {
+        return match fs::read(path) {
+            Ok(content) => Ok(sha256::digest(content)),
+            Err(err) => {
+                Err(HashError(format!("Couldn't open file: {}", err.to_string())))
+            }
+        }
+    } 
+
+    Err(HashError(String::from("Specified path is not a file!")))
+}
+
+fn add_to_dict(path: &Path, recursive: bool, level: i8) -> HashMap<String, String>{
+    let mut dictionary: HashMap<String, String> = HashMap::new();
+
+    if path.is_dir() {
+        if recursive || level == 0 {
+            for entry in read_dir(path).unwrap() {
+                match entry {
+                    Ok(v) => {
+                        let sub_dict = add_to_dict(&v.path(), recursive, level+1);
+                        dictionary.extend(sub_dict);
+                    },
+                    Err(e) => eprintln!("Error while reading {}: {}", path.to_string_lossy(), e)
+                }
+            }
+        }
+    } else {
+        match hash_file(path) {
+            Ok(hash) => {
+                dictionary.insert(String::from(path.to_string_lossy()), hash);
+            },
+            Err(e) => eprintln!("Error while adding {} to the dictionary: {}", path.to_string_lossy(), e)
+        }
+    }
+
+    dictionary
 }
 
 fn main() {
@@ -27,25 +76,26 @@ fn main() {
     let mut dictionary: HashMap<String, String> = HashMap::new();
 
     match &cli.command {
-        Commands::Dict{ output, recursive: _ } => {
-            let path = Path::new(".");
-            for entry in read_dir(path).unwrap() {
-                let file = entry.unwrap().path();
-                if file.is_file() {
-                    dictionary.insert(String::from(file.to_string_lossy()), hash_file(&file));
-                }
-            }
-            let serialized = serde_json::to_string(&dictionary).unwrap();
-            if let Err(err) = fs::write(output, serialized) {
-                println!("Error writing to output file: {}", err.to_string());
-            }
+        Commands::Dict{ output, recursive } => {
+            let p = Path::new("test");
+            println!("{:?}", add_to_dict(p, *recursive, 0));
+
+            // let path = Path::new(".");
+            // for entry in read_dir(path).unwrap() {
+            //     let file = entry.unwrap().path();
+            //     if file.is_file() {
+            //         dictionary.insert(String::from(file.to_string_lossy()), hash_file(&file));
+            //     }
+            // }
+            // let serialized = serde_json::to_string(&dictionary).unwrap();
+            // if let Some(output_path) = output {
+            //     if let Err(err) = fs::write(output_path, serialized) {
+            //         println!("Error writing to output file: {}", err.to_string());
+            //     }
+            // } else {
+            //     print!("{}", serialized);
+            //     let _ = io::stdout().flush();
+            // }
         },
     };
-}
-
-fn hash_file(path: &Path) -> String {
-    match fs::read(path) {
-        Ok(content) => sha256::digest(content),
-        Err(err) => String::from(format!("Couldn't open file: {}", err.to_string()))
-    }
 }
